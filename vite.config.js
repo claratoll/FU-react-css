ff2f65e7b8a289aa06de822845ad11e04d73dfd6
Clara Toll <43537329+claratoll@users.noreply.github.com>
2023-09-28 15:10:11 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "git-FU-react-css",
+  base: "FU-react-css",
   plugins: [react()],
 });
